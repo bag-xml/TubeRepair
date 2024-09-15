3ed7992d207fa59a4d16e52cf00e8d550f3be850
Savefade <81902561+Savefade@users.noreply.github.com>
2024-09-15 14:14:41 +0300

    Update configuration.php

--- src/backend/TubeRepair/configuration.php
+++ src/backend/TubeRepair/configuration.php
@@ -4,7 +4,7 @@ $underMaintenance = false; // put the server in maintenance
 //config
 $skipHandshakeLogin = false; // skip the handshake when opening the app
 $invidiousURL = "invidious.fdn.fr"; // TubeRepair uses an invidious endpoint to get youtube videos. if the default doesnt work please find one here: https://docs.invidious.io/instances/
-$APIkey = "AIzaSyDXpueywmjWFGVSjrn6031NaTqZszxI_Rw"; // insert your api key here!
+$APIkey = ""; // insert your api key here!
 $baseURL = "www.google.com"; // enter your url where this is hosted NOTE: without any / with http/s
 $serverScriptDirectory = "TubeRepair"; // please input the correct directory where your server scripts are located!
 $APIurl = "www.googleapis.com"; //incase they change the api to a new url you can input it here
